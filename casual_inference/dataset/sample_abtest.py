b43b42617fc0246f461c4c62b9d7ee4010064f6c
Shinya Yaginuma <s-yaginuma@mercari.com>
2022-08-26 09:59:48 +0900

    implement sample A/B test data generator

---
+++ casual_inference/dataset/sample_abtest.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+
+def create_sample_ab_result(
+    n_variant: int = 2, sample_size: int = 1000, metric_base: float = 0.1, simulated_lift: list[float] = []
+) -> pd.DataFrame:
+    """
+    Returns
+    -------
+    pd.DataFrame
+        Sample A/B test result contains columns as follows
+            - rand_unit: randomization unit. something like user_id, session_id, ...
+            - variant: which pattern the unit was assigned. assuming 1 is the control group.
+            - metric: a binary metric assuming the target metric of the A/B test. e.g., Click, Purchase, ...
+    """
+    if n_variant <= 1:
+        raise ValueError("n_variant should be more than or equal 2.")
+    if len(simulated_lift) >= 1 and len(simulated_lift) != n_variant - 1:
+        raise ValueError("The length of simulated_lift doesn't equal to the number of treatment variants.")
+
+    ab_result = pd.DataFrame()
+    ab_result["rand_unit"] = [i for i in range(sample_size)]
+    ab_result["variant"] = np.random.choice(a=[i + 1 for i in range(n_variant)], size=sample_size)
+    ab_result["metric"] = 0
+
+    if len(simulated_lift) == 0:
+        simulated_lift = [0.005 * i for i in range(n_variant) if i != 0]
+    simulated_lift.insert(0, 0)  # insert lift of control variant for convenience
+    for i in range(n_variant):
+        ab_result.loc[ab_result["variant"] == i + 1, "metric"] = np.random.binomial(
+            n=1, p=metric_base * (1 + simulated_lift[i]), size=ab_result.loc[ab_result["variant"] == i + 1].shape[0]
+        )
+    return ab_result
