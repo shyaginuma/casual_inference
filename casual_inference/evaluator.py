3653649f5e6b09fc625fb5d1ad2a8071e6dcc93d
Shinya Yaginuma <s-yaginuma@mercari.com>
2022-09-04 12:37:49 +0900

    Implement summarization of the result of A/B test (#3)

--- casual_inference/evaluator.py
+++ casual_inference/evaluator.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import ttest_ind_from_stats
+import plotly
+import plotly.express as px
+from scipy.stats import t, ttest_ind_from_stats
 
 
 class ABTestEvaluator:
@@ -56,25 +58,24 @@ class ABTestEvaluator:
         )
         stats = means.merge(vars, on=[variant_col, "metric"]).merge(counts, on=[variant_col, "metric"])
         stats["std"] = np.sqrt(stats["var"])
+        stats["stderr"] = np.sqrt(stats["var"] / stats["count"])
         stats = stats.merge(stats.query(f"{variant_col} == 1"), on="metric", suffixes=["", "_c"]).drop(
             "variant_c", axis=1
         )
 
         stats["abs_diff_mean"] = stats["mean"] - stats["mean_c"]
-        stats["abs_diff_std"] = np.sqrt(
-            np.power(stats["std"], 2) / stats["count"] + np.power(stats["std_c"], 2) / stats["count_c"]
-        )
+        stats["abs_diff_std"] = np.sqrt(stats["stderr"] ** 2 + stats["stderr_c"] ** 2)
         stats["rel_diff_mean"] = stats["mean"] / stats["mean_c"] - 1
 
         # see: https://arxiv.org/pdf/1803.06336.pdf
-        stats["rel_diff_std"] = np.sqrt(
-            (
-                stats["var"] / stats["count"]
-                + (stats["mean"] ** 2 / stats["mean_c"] ** 2) * stats["var_c"] / stats["count_c"]
-            )
+        stats["rel_diff_std"] = (
+            np.sqrt((stats["stderr"] ** 2 + (stats["mean"] ** 2 / stats["mean_c"] ** 2) * stats["stderr_c"] ** 2))
             / stats["mean_c"]
         )
         stats["t_value"] = stats["abs_diff_mean"] / stats["abs_diff_std"]
+        stats["dof"] = stats["abs_diff_std"] ** 4 / (
+            stats["stderr"] ** 4 / (stats["count"] - 1) + stats["stderr_c"] ** 4 / (stats["count_c"] - 1)
+        )
         stats["p_value"] = stats.apply(
             lambda x: ttest_ind_from_stats(
                 mean1=x["mean"],
@@ -89,7 +90,108 @@ class ABTestEvaluator:
         )
         self.stats = stats
 
-    def summary(self) -> None:
+    def summary_table(self, p_threshold: float = 0.05) -> pd.DataFrame:
+        """return statistics summary.
+
+        In the future, styling dataframe or add another visualization can be considered.
+
+        Parameters
+        ----------
+        p_threshold : float, optional
+            significance level, by default 0.05
+
+        Returns
+        -------
+        pd.DataFrame
+            stats summary
+        """
         if self.stats is None:
             raise ValueError("A/B test statistics haven't been calculated. Please call evaluate() in advance.")
-        pass
+
+        stats = self.stats.copy(deep=True)
+        significance, abs_ci_width, rel_ci_width = self._eval_significance(p_threshold)
+        stats["significance"] = significance
+        stats["abs_ci_width"] = abs_ci_width
+        stats["rel_ci_width"] = rel_ci_width
+        for diff_type in ["abs", "rel"]:
+            stats[f"ci_{diff_type}_diff"] = stats.apply(
+                lambda x: (
+                    x[f"{diff_type}_diff_mean"] - x[f"{diff_type}_ci_width"],
+                    x[f"{diff_type}_diff_mean"] + x[f"{diff_type}_ci_width"],
+                ),
+                axis=1,
+            )
+            del stats[f"{diff_type}_ci_width"]
+        return_cols = [col for col in stats.columns if col[-2:] != "_c"]
+        return stats.loc[:, return_cols]
+
+    def summary_barplot(
+        self, p_threshold: float = 0.05, diff_type: str = "rel", display_ci: bool = True
+    ) -> plotly.graph_objs.Figure:
+        """plot impact and confidence interval for each metric
+
+        Parameters
+        ----------
+        p_threshold : float, optional
+            significance level, by default 0.05
+
+        Returns
+        -------
+        plotly.graph_objs.Figure
+        """
+        if self.stats is None:
+            raise ValueError("A/B test statistics haven't been calculated. Please call evaluate() in advance.")
+        if diff_type not in ["rel", "abs"]:
+            raise ValueError("Specified diff type is invalid.")
+
+        stats = self.stats.copy(deep=True)
+        significance, abs_ci_width, rel_ci_width = self._eval_significance(p_threshold)
+        stats["significant"] = significance
+        stats["abs_ci_width"] = abs_ci_width
+        stats["rel_ci_width"] = rel_ci_width
+
+        viz_options = {
+            "data_frame": stats,
+            "x": f"{diff_type}_diff_mean",
+            "y": "metric",
+            "facet_col": "variant",
+            "color": "significant",
+            "color_discrete_map": {"up": "#54A24B", "down": "#E45756", "unclear": "silver"},
+        }
+        if display_ci:
+            viz_options["error_x"] = f"{diff_type}_ci_width"
+
+        g = px.bar(**viz_options)
+        g.show()
+        return g
+
+    def _eval_significance(self, p_threshold: float = 0.05) -> tuple[pd.Series, pd.Series, pd.Series]:
+        """evaluate statistical significance & return information related to that.
+
+        Parameters
+        ----------
+        p_threshold : float, optional
+            _description_, by default 0.05
+
+        Returns
+        -------
+        tuple[pd.Series]
+            includes three pd.Series as follows:
+            - statistical significance
+            - confidence interval width of absolute difference
+            - confidence interval width of relative difference
+        """
+
+        significance = self.stats.apply(
+            lambda x: "up"
+            if x["p_value"] <= p_threshold and x["t_value"] > 0
+            else "down"
+            if x["p_value"] <= p_threshold and x["t_value"] < 0
+            else "unclear",
+            axis=1,
+        )
+
+        abs_ci_width = t.ppf(1 - p_threshold / 2, self.stats["dof"]) * self.stats["abs_diff_std"]
+        rel_ci_width = t.ppf(1 - p_threshold / 2, self.stats["dof"]) * self.stats["rel_diff_std"]
+
+        return significance, abs_ci_width, rel_ci_width
---
+++ tests/test_evaluator.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pytest
+
+from casual_inference.dataset import sample_abtest
+from casual_inference.evaluator import ABTestEvaluator
+
+
+@pytest.fixture
+def prepare_evaluator() -> ABTestEvaluator:
+    sample_data = sample_abtest.create_sample_ab_result(
+        n_variant=4, sample_size=1000000, simulated_lift=[0.01, 0.05, -0.05]
+    )
+    evaluator = ABTestEvaluator()
+    evaluator.evaluate(sample_data, unit_col="rand_unit", variant_col="variant", metrics=["metric_bin", "metric_cont"])
+    return evaluator
+
+
+class TestABTestEvaluator:
+    def prepare_evaluator(self):
+        return ABTestEvaluator()
+
+    def test_evaluate(self, prepare_evaluator):
+        evaluator = prepare_evaluator
+
+        stats_cols = [
+            "variant",
+            "metric",
+            "mean",
+            "var",
+            "count",
+            "std",
+            "stderr",
+            "mean_c",
+            "var_c",
+            "count_c",
+            "std_c",
+            "stderr_c",
+            "abs_diff_mean",
+            "abs_diff_std",
+            "rel_diff_mean",
+            "rel_diff_std",
+            "t_value",
+            "dof",
+            "p_value",
+        ]
+
+        assert (evaluator.stats.columns == stats_cols).all()
+        assert evaluator.stats["p_value"].min() > 0.0
+        assert evaluator.stats["p_value"].max() <= 1.0
+        assert (evaluator.stats.query("variant == 1")["mean"] == evaluator.stats.query("variant == 1")["mean_c"]).all()
+        assert (evaluator.stats["var"] >= 0).all()
+        assert (evaluator.stats["count"] >= 0).all()
+        assert (evaluator.stats["std"] >= 0).all()
+        assert (evaluator.stats["std"] >= evaluator.stats["stderr"]).all()
+        assert (evaluator.stats["abs_diff_std"] >= 0).all()
+        assert (evaluator.stats["abs_diff_std"] >= evaluator.stats["stderr"]).all()
+        assert (evaluator.stats["rel_diff_std"] >= 0).all()
+        assert (np.sign(evaluator.stats["abs_diff_mean"]) == np.sign(evaluator.stats["rel_diff_mean"])).all()
+        assert (np.sign(evaluator.stats["abs_diff_mean"]) == np.sign(evaluator.stats["t_value"])).all()
+
+    @pytest.mark.parametrize("p_threshold", (0.01, 0.05, 0.1))
+    def test_summary_table(self, p_threshold, prepare_evaluator):
+        evaluator: ABTestEvaluator = prepare_evaluator
+        summary = evaluator.summary_table(p_threshold=p_threshold)
+
+        assert "significance" in summary.columns
+        assert "ci_abs_diff" in summary.columns
+        assert "ci_rel_diff" in summary.columns
+        assert summary.query(f"p_value <= {p_threshold}")["significance"].isin(["up", "down"]).all()
+        assert (
+            summary.query(f"p_value <= {p_threshold}")["ci_abs_diff"]
+            .map(lambda x: True if x[1] < 0 or x[0] > 0 else False)
+            .all()
+        )
+        assert (
+            summary.query(f"p_value <= {p_threshold}")["ci_rel_diff"]
+            .map(lambda x: True if x[1] < 0 or x[0] > 0 else False)
+            .all()
+        )
+
+    @pytest.mark.parametrize("diff_type", ("rel", "abs"))
+    def test_summary_barplot(self, diff_type, prepare_evaluator):
+        evaluator: ABTestEvaluator = prepare_evaluator
+        evaluator.summary_barplot(diff_type=diff_type)
