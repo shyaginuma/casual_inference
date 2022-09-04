0b7aeebe84f8ba4f6d4fa846bdbfa5ca6aaa9c0a
Shinya Yaginuma <s-yaginuma@mercari.com>
2022-09-04 12:21:57 +0900

    write test about summary table

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -15,35 +15,35 @@ def prepare_evaluator() -> ABTestEvaluator:
     return evaluator
 
 
-stats_cols = [
-    "variant",
-    "metric",
-    "mean",
-    "var",
-    "count",
-    "std",
-    "stderr",
-    "mean_c",
-    "var_c",
-    "count_c",
-    "std_c",
-    "stderr_c",
-    "abs_diff_mean",
-    "abs_diff_std",
-    "rel_diff_mean",
-    "rel_diff_std",
-    "t_value",
-    "dof",
-    "p_value",
-]
-
-
 class TestABTestEvaluator:
     def prepare_evaluator(self):
         return ABTestEvaluator()
 
     def test_evaluate(self, prepare_evaluator):
         evaluator = prepare_evaluator
+
+        stats_cols = [
+            "variant",
+            "metric",
+            "mean",
+            "var",
+            "count",
+            "std",
+            "stderr",
+            "mean_c",
+            "var_c",
+            "count_c",
+            "std_c",
+            "stderr_c",
+            "abs_diff_mean",
+            "abs_diff_std",
+            "rel_diff_mean",
+            "rel_diff_std",
+            "t_value",
+            "dof",
+            "p_value",
+        ]
+
         assert (evaluator.stats.columns == stats_cols).all()
         assert evaluator.stats["p_value"].min() > 0.0
         assert evaluator.stats["p_value"].max() <= 1.0
@@ -61,8 +61,15 @@ class TestABTestEvaluator:
     @pytest.mark.parametrize("p_threshold", (0.01, 0.05, 0.1))
     def test_summary_table(self, p_threshold, prepare_evaluator):
         evaluator: ABTestEvaluator = prepare_evaluator
-        evaluator.summary_table(p_threshold=p_threshold)
-        # TODO: Add test to check statistics
+        summary = evaluator.summary_table(p_threshold=p_threshold)
+
+        assert "significance" in summary.columns
+        assert "ci_abs_diff" in summary.columns
+        assert "ci_rel_diff" in summary.columns
+        assert summary.query(f"p_value <= {p_threshold}")["significance"].isin(["up", "down"]).all()
+        assert summary.query(f"p_value <= {p_threshold}")["ci_abs_diff"].map(lambda x: True if x[1] < 0 or x[0] > 0 else False).all()
+        assert summary.query(f"p_value <= {p_threshold}")["ci_rel_diff"].map(lambda x: True if x[1] < 0 or x[0] > 0 else False).all()
+
 
     @pytest.mark.parametrize("diff_type", ("rel", "abs"))
     def test_summary_barplot(self, diff_type, prepare_evaluator):
