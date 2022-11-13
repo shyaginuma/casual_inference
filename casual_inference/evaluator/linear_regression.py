cc62109667be593f8b32493afbddc971138daebc
Shinya Yaginuma <s-yaginuma@mercari.com>
2022-11-13 12:02:51 +0900

    build linear regression model

--- casual_inference/evaluator/linear_regression.py
+++ casual_inference/evaluator/linear_regression.py
@@ -1,19 +1,60 @@
 import pandas as pd
 import plotly.graph_objs as go
-from sklearn.preprocessing import StandardScaler
+import statsmodels.formula.api as smf
 from typing_extensions import Self
 
 from .base import BaseEvaluator
 
+
 class LinearRegressionEvaluator(BaseEvaluator):
-    """Evaluate treatment impact by Linear Regression
-    """
+    """Evaluate treatment impact by Linear Regression"""
 
     def __init__(self) -> None:
         super().__init__()
+        self.models: dict[str, smf.ols] = dict()
 
-    def evaluate(self, data: pd.DataFrame, unit_col: str, metrics: list[str]) -> Self:
-        return super().evaluate(data, unit_col, metrics)
+    def evaluate(
+        self,
+        data: pd.DataFrame,
+        unit_col: str,
+        metrics: list[str],
+        treatment_col: str = "treatment",
+        covariates: list[str] = [],
+    ) -> Self:
+        """Evaluate impact by Linear Regression model
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Dataframe has randomization unit column, and metrics columns. The data should have been aggregated by the randomization unit.
+        unit_col : str
+            A column name stores the randomization unit. something like user_id, session_id, ...
+        metrics : list[str]
+            Columns stores metrics you want to evaluate.
+        treatment_col : str
+            A column name stores information if the unit received treatment or not.
+            Stored value should be binary:
+                - 0: did not receive treatment
+                - 1: received treatment
+        covariates : list[str], optional
+            column names of covariate variables you want to use for the analysis, by default []
+
+        Returns
+        -------
+        Self
+            Evaluator object has statistics calculated
+        """
+        self._validate_passed_data(data, unit_col, metrics)
+        if data[treatment_col].unique() != [0, 1]:
+            raise ValueError("The treatment value should be binary.")
+
+        covariates_str = "+ ".join(covariates)
+
+        for metric in metrics:
+            model = smf.ols(formula=f"{metric} ~ {treatment_col} + {covariates_str}", data=data).fit()
+            self.models[metric] = model
+
+        return self
 
     def summary_table(self) -> pd.DataFrame:
         return super().summary_table()
