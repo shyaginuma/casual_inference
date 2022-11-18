b0078070e9f1ac2fb4bbafc0a265f95d016dfed0
Shinya Yaginuma <s-yaginuma@mercari.com>
2022-11-18 10:02:26 +0900

    Add sample data generation function for advanced causal inference technique

--- casual_inference/dataset/__init__.py
+++ casual_inference/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .sample_abtest import create_sample_ab_result
+from .sample_biased import create_sample_biased
 
-__all__ = ["create_sample_ab_result"]
+__all__ = ["create_sample_ab_result", "create_sample_biased"]
