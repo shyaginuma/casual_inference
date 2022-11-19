644edd50344966557c397aeb8f3d1c87cba65841
Shinya Yaginuma <s-yaginuma@mercari.com>
2022-11-19 18:51:59 +0900

    implement summary_table()

--- casual_inference/evaluator/linear_regression.py
+++ casual_inference/evaluator/linear_regression.py
@@ -46,19 +46,27 @@ class LinearRegressionEvaluator(BaseEvaluator):
             Evaluator object has statistics calculated
         """
         self._validate_passed_data(data, unit_col, metrics)
-        if data[treatment_col].unique() != [0, 1]:
+        if set(data[treatment_col].unique()) != {0, 1}:
             raise ValueError("The treatment value should be binary.")
 
-        covariates_str = "+ ".join(covariates)
+        covariates_str = ""
+        if len(covariates) > 0:
+            covariates_str = "+ " + "+ ".join(covariates)
 
         for metric in metrics:
-            model = smf.ols(formula=f"{metric} ~ {treatment_col} + {covariates_str}", data=data).fit()
+            model = smf.ols(formula=f"{metric} ~ {treatment_col} {covariates_str}", data=data).fit()
             self.models[metric] = model
+            stats_partial = pd.read_html(model.summary().tables[1].as_html(), header=0, index_col=0)[0].reset_index()
+            stats_partial["metric"] = metric
+            self.stats = pd.concat([self.stats, stats_partial], axis=1)
+        self.stats = self.stats.query(f"index == '{treatment_col}'").reset_index(drop=True).drop(treatment_col, axis=1)
 
         return self
 
     def summary_table(self) -> pd.DataFrame:
-        return super().summary_table()
+        self._validate_evaluate_executed()
+        stats = self.stats.copy(deep=True)
+        return stats
 
     def summary_plot(self) -> go.Figure:
         return super().summary_plot()
