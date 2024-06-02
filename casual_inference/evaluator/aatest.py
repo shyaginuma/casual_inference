60226f2680b081c906dceb26566dff2582f40bda
Shinya Yaginuma <yaginuuun@gmail.com>
2024-06-02 10:46:46 +0900

    Add `CustomMetric` model and expand types can be received on `metrics` arg

--- casual_inference/evaluator/aatest.py
+++ casual_inference/evaluator/aatest.py
@@ -5,6 +5,7 @@ import plotly.graph_objs as go
 from scipy.stats import kstest
 from typing_extensions import Self
 
+from ..model import CustomMetric
 from ..statistical_testing import t_test
 from .base import BaseEvaluator
 
@@ -30,7 +31,7 @@ class AATestEvaluator(BaseEvaluator):
         self.sample_rate = sample_rate
 
     # ignore mypy error temporary, because the "Self" type support on mypy is ongoing. https://github.com/python/mypy/pull/11666
-    def evaluate(self, data: pd.DataFrame, unit_col: str, metrics: list[str]) -> Self:  # type: ignore
+    def evaluate(self, data: pd.DataFrame, unit_col: str, metrics: list[str | CustomMetric]) -> Self:  # type: ignore
         """split data n times, and calculate statistics n times, then store it as an attribute.
 
         Parameters
--- casual_inference/evaluator/abtest.py
+++ casual_inference/evaluator/abtest.py
@@ -9,6 +9,7 @@ import plotly.graph_objs as go
 from scipy.stats import chisquare
 from typing_extensions import Self
 
+from ..model import CustomMetric
 from ..statistical_testing import eval_ttest_significance, t_test
 from .base import BaseEvaluator
 
@@ -41,7 +42,7 @@ class ABTestEvaluator(BaseEvaluator):
         self,
         data: pd.DataFrame,
         unit_col: str,
-        metrics: list[str],
+        metrics: list[str | CustomMetric],
         variant_col: str = "variant",
         segment_col: Optional[str] = None,
     ) -> Self:  # type: ignore
